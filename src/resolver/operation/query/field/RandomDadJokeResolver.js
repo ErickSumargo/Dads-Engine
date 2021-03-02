38162038800b72b8f10b2752faff4ac019ef082b
ErickSumargo <ericksumargo@outlook.com>
2021-03-02 08:24:22 +0700

    Remove Authentication at Startup (#4)

--- src/resolver/operation/query/field/RandomDadJokeResolver.js
+++ src/resolver/operation/query/field/RandomDadJokeResolver.js
@@ -1,13 +1,18 @@
+const { ApolloError } = require('apollo-server');
+
 const mapper = require('../../../../mapper/DadJokeMapper')
 
-const resolver = async (_, __, { dataSources }) => {
-    try {
-        const response = await dataSources.api.dadJokes.fetchRandomDadJoke();
-        return mapper(response)
-    } catch (error) {
-        console.log(error)
-        return error
+const resolver = async (_, __, { verified, dataSources }) => {
+    if (verified) {
+        try {
+            const response = await dataSources.api.dadJokes.fetchRandomDadJoke();
+            return mapper(response)
+        } catch (error) {
+            console.log(error)
+            return error
+        }
     }
+    throw new ApolloError('Unauthorized');
 };
 
 module.exports = resolver;
